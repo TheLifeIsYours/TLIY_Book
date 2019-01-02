9ece5e43d3919fd8026f35a1e7a40efe8c0a18a5
Mats Larsen <7der54@gmail.com>
2019-01-02 18:01:24 +0100

    Cleaned up some code, made it more modulare

--- TLIY_Book.js
+++ TLIY_Book.js
@@ -3,39 +3,67 @@ const path = require('path');
 
 module.exports = class TLIY_Book {
     constructor(){
-        this.dictionaryPath = './data/dictionaries/lastDictionary.json';
-        this.dictionary;
-
+        //setup dictionary from all scans, to be used for sentence making
+        this.collectedDictionariesPath = './data/dictionaries/collectedDictionaries.json';
+        this.collectedDictionary = this.readDictionary(this.collectedDictionariesPath);
+        
         //setup dictionary from last scan, if no scan takes place
-        this.readDictionary();
+        this.latesDictionaryPath = './data/dictionaries/latestDictionary.json';
+        this.latestDictionary = this.readDictionary(this.latesDictionaryPath);
+
+        this.currentDictionaryPath;
+        this.currentDictionary;
     }
 
-    readDictionary(){
-        this.dictionary = JSON.parse(fs.readFileSync(this.dictionaryPath));
-    }
-    
-    writeDictionary(){
-        fs.writeFileSync(this.dictionaryPath, JSON.stringify(this.dictionary));
+    setCurrentDictionary(dictionaryPath) {
+        this.currentDictionaryPath = dictionaryPath;
+        this.currentDictionary = this.readDictionary(dictionaryPath);
     }
 
-    updateLastDictionary(){
-        fs.writeFileSync('./data/dictionaries/lastDictionary.json', JSON.stringify(this.dictionary));
-    }
-
-    setDictionaryPath(path) {
-        this.dictionaryPath = path;
-    }
-    
-    makeDictionaryFile() {
-        if(!fs.existsSync(this.dictionaryPath)){
-            fs.writeFileSync(this.dictionaryPath, "{}");
+    readDictionary(dictionaryPath){
+        if(!fs.existsSync(dictionaryPath)){
+            fs.writeFileSync(dictionaryPath, "{}");
         }
+        return JSON.parse(fs.readFileSync(dictionaryPath));
+    }
+    
+    writeDictionary(dictionary, dictionaryPath){
+        fs.writeFileSync(dictionaryPath, JSON.stringify(dictionary));
+    }
+
+    searchDictionary(dictionary, word){
+        return dictionary[word] != undefined ? {found: true, entry: dictionary[word]} : {found: false};
+    }
+
+    updateCollectedDictionary(dictionary) {
+        dictionary.forEach((entry) => {
+            this.collectedDictionary.forEach((collectedEntry) => {
+                let wordInfo = this.searchDictionary(this.collectedDictionary, entry._this);
+                
+                if(wordInfo.found){
+                    entry.associates.forEach((associate) => {
+                        let associateExist = false;
+                        collectedEntry.associates.forEach((collectedAssociate) => {
+                            if(associate == collectedAssociate){
+                                associateExist = true;
+                                collectedAssociate.score++;
+                            }
+                        });
+
+                        if(!associateExist){
+                            collectedEntry.associates.push({_this: refined[index+1], score: 1});
+                        }
+                    })
+                } else if (wordInfo.found == false) {
+                    this.currentDictionary[word] = {"_this": word, "associates": [{"_this": refined[index+1], "score": 1}]};
+                    //console.log(`added new word "${word}" to dictionary`);
+                }
+            });
+        });
     }
 
     scanText(textPath){
-        this.setDictionaryPath(`./data/dictionaries/${path.basename(textPath).replace(/\.\w+$/gm, ".json")}`);
-        this.makeDictionaryFile();
-        this.readDictionary();
+        this.setCurrentDictionary(`./data/dictionaries/${path.basename(textPath).replace(/\.\w+$/gm, ".json")}`);
 
         let text = fs.readFileSync(textPath, 'utf8');
         let refined = text.toLocaleLowerCase().match(/[\w'-]+/gm);
@@ -51,7 +79,7 @@ module.exports = class TLIY_Book {
             currentTime = new Date();
             console.log(`\n\n\n\n\n`);
             //console.log(Searching for word "${word}"`);
-            let wordInfo = this.searchDictionary(word);
+            let wordInfo = this.searchDictionary(this.currentDictionary, word);
 
             if(wordInfo.found){
                 //console.log(`found word entry "${wordInfo.entry._this}" \nwith ${wordInfo.entry.associates != undefined ? wordInfo.entry.associates.length : 0} associates`);
@@ -64,16 +92,16 @@ module.exports = class TLIY_Book {
                 })
 
                 if(!associateExist){
-                    this.dictionary[word].associates.push({_this: refined[index+1], score: 1});
+                    this.currentDictionary[word].associates.push({_this: refined[index+1], score: 1});
                     //console.log(`added new associate: "${refined[index+1]}" to word "${word}"`);
                 }
             } else if (wordInfo.found == false) {
-                this.dictionary[word] = {"_this": word, "associates": [{"_this": refined[index+1], "score": 1}]};
+                this.currentDictionary[word] = {"_this": word, "associates": [{"_this": refined[index+1], "score": 1}]};
                 //console.log(`added new word "${word}" to dictionary`);
             }
             
-            console.log(`Scanning text to dictionary ${path.basename(this.dictionaryPath)}`);
-            this.writeDictionary();
+            console.log(`Scanning text to dictionary ${path.basename(this.currentDictionaryPath)}`);
+            this.writeDictionary(this.currentDictionary, this.currentDictionaryPath);
             
             if(averageTime.length >= 256){
                 averageTime.splice(0, 1);
@@ -86,24 +114,23 @@ module.exports = class TLIY_Book {
             newCountDownTime = new Date((estimateTime.getTime() + startTime.getTime()) - new Date().getTime());
             countDownTime = newCountDownTime.getTime() <= countDownTime.getTime() ? newCountDownTime : countDownTime;
 
-            console.log(`Processing progress: ${Number(Math.round(Number(index * 100/refined.length)+"e2")+"e-2")}% \nTime remaining ${countDownTime.getHours()-1}:${countDownTime.getMinutes()}:${countDownTime.getSeconds()}`);
+            console.log(`Processing progress: ${Number(Math.ceil(Number(index * 100/refined.length)+"e2")+"e-2")}% \nTime remaining ${countDownTime.getHours()-1}:${countDownTime.getMinutes()}:${countDownTime.getSeconds()}`);
         });
 
-        this.updateLastDictionary();
-    }
-
-    searchDictionary(word){
-        return this.dictionary[word] != undefined ? {found: true, entry: this.dictionary[word]} : {found: false};
+        this.writeDictionary(this.currentDictionary, this.latesDictionaryPath);
+        this.updateCollectedDictionary(this.currentDictionary);
     }
 
     makeSentence(words, startWord, highestScore){
+        this.setCurrentDictionary(this.latesDictionaryPath);
+
         let res = "";
-        startWord = this.dictionary[startWord] != undefined ? this.dictionary[startWord]._this : false;
+        startWord = this.currentDictionary[startWord] != undefined ? this.currentDictionary[startWord]._this : false;
         highestScore = highestScore != undefined ? true : false;
 
         if(!startWord){
             console.error("Could not find start word in dictionary", "Picking a random start word");
-            startWord = this.getRandomObjectValue(this.dictionary)._this;
+            startWord = this.getRandomObjectValue(this.currentDictionary)._this;
         }
 
         console.log(startWord);
@@ -112,7 +139,7 @@ module.exports = class TLIY_Book {
 
         let currWord = startWord;
         for(let i = 0; i < words; i++){
-            let wordAssosiacets = this.dictionary[currWord].associates;
+            let wordAssosiacets = this.currentDictionary[currWord].associates;
 
             if(highestScore){
                 let highestScoring = 0;
